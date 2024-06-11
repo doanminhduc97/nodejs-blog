8ff4b1286f02ea0152fbed9d0d75f451251ffe8e
doanminhduc1007 <ducshooter.luvina@gmail.com>
2024-06-11 21:19:37 +0700

    add bootstrap

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const  handlebars  = require('express-handlebars');
+const hbs = handlebars.create({
+  extname: '.hbs'
+});
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Join folder public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// // Template engine
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/new', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
+
