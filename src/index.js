e1eeb194652563ed8238b8aa06e7806977fa7587
doanminhduc1007 <ducshooter.luvina@gmail.com>
2024-07-15 21:16:17 +0700

    MVC Routes & Controllers | Mô hình MVC

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const hbs = handlebars.create({
 const path = require('path');
 const app = express();
 const port = 3000;
-
+const route = require('./routes');
 
 app.use(express.urlencoded({
   extended: true
@@ -25,25 +25,9 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.render('search');
-});
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
-
